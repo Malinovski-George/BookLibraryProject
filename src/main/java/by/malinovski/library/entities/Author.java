93345e7c58bf80486bee8236300750805a8550e2
George Malinovski <MalinovskiGeorge@gmail.com>
2017-07-02 21:40:16 +0200

    Add Hibernate

---
+++ src/main/java/by/malinovski/library/entities/Author.java
@@ -0,0 +1,69 @@
+package by.malinovski.library.entities;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Date;
+
+/**
+ * Created by Gia on 02.07.2017.
+ */
+@Entity
+public class Author {
+    private long id;
+    private String fio;
+    private Date birthday;
+
+    @Id
+    @Column(name = "id")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "fio")
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    @Basic
+    @Column(name = "birthday")
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        if (id != author.id) return false;
+        if (fio != null ? !fio.equals(author.fio) : author.fio != null) return false;
+        if (birthday != null ? !birthday.equals(author.birthday) : author.birthday != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (fio != null ? fio.hashCode() : 0);
+        result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
+        return result;
+    }
+}
