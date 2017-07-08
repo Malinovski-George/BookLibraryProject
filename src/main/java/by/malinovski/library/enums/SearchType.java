56a4be73693dbc63a4d3baa09103cc146a049afb
George Malinovski <MalinovskiGeorge@gmail.com>
2017-07-08 21:59:17 +0200

    Upd

---
+++ src/main/java/by/malinovski/library/enums/SearchType.java
@@ -0,0 +1,7 @@
+package by.malinovski.library.enums;
+
+public enum SearchType {
+
+    AUTHOR,
+    TITLE
+}
--- src/main/java/by/malinovski/library/objects/Utils.java
+++ src/main/java/by/malinovski/library/objects/Utils.java
@@ -1,15 +1,59 @@
 package by.malinovski.library.objects;
 
 
+import by.malinovski.library.enums.SearchType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
+
+import javax.faces.context.FacesContext;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class Utils {
 
-    private Character[] letters = new Character[]{'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'};
+    private Map<String, SearchType> searchTypeList = new HashMap<String, SearchType>();
+    private SearchType selectedSearchType = SearchType.TITLE;// значение по-умолчанию
+    @Autowired
+    private MessageSource msg;
 
-    public Character[] getLetters() {
-        return letters;
+
+    public Map<String, SearchType> getSearchTypeList() {
+        searchTypeList.clear();
+        searchTypeList.put(msg.getMessage("author_name", null, FacesContext.getCurrentInstance().getViewRoot().getLocale()), SearchType.AUTHOR);
+        searchTypeList.put(msg.getMessage("book_name", null, FacesContext.getCurrentInstance().getViewRoot().getLocale()), SearchType.TITLE);
+        return searchTypeList;
     }
+
+    public SearchType getSelectedSearchType() {
+        return selectedSearchType;
+    }
+
+    public void setSearchTypeList(Map<String, SearchType> searchTypeList) {
+        this.searchTypeList = searchTypeList;
+    }
+
+
+
+
+    private Character[] lettersRU = new Character[]{'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'};
+
+    public Character[] getLettersRU() {
+        return lettersRU;
+    }
+
+    private Character[] lettersEN = new Character[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    public Character[] getLettersEN() {
+        return lettersEN;
+    }
+
+
+
+
+
+
+
 
 }
