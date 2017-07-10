6bbc963f5ecd7f7f95ffaf27afc444c50b37dce3
George Malinovski <MalinovskiGeorge@gmail.com>
2017-07-10 17:04:37 +0200

    Add book search by letter

--- src/main/java/by/malinovski/library/objects/LibraryFacade.java
+++ src/main/java/by/malinovski/library/objects/LibraryFacade.java
@@ -3,23 +3,30 @@ package by.malinovski.library.objects;
 import by.malinovski.library.dao.interfaces.BookDAO;
 import by.malinovski.library.entities.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@Scope("singleton")
 public class LibraryFacade {
 
-    private BookDAO bookDAO;
     private List<Book> books;
-
     @Autowired
-    public void setBookDAO(BookDAO bookDAO) {
-        this.bookDAO = bookDAO;
-        books = bookDAO.getBooks();
-    }
+    private BookDAO bookDAO;
+    @Autowired
+    private SearchCriteria searchCriteria;
+
 
     public List<Book> getBooks() {
+        if (books == null){
+            books = bookDAO.getBooks();
+        }
         return books;
+    }
+
+    public void searchBooksByLetter() {
+        books = bookDAO.getBooks(searchCriteria.getLetter());
     }
 }
